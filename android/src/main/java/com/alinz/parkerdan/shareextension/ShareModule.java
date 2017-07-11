27cef78bd1fec385b4108e76f265ccfe4916a63e
Bart Arribe <barthelemy.arribe@mediapps.io>
2017-07-11 15:02:55 +0200

    Reindent properly two Java files

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -21,70 +21,69 @@ import java.util.ArrayList;
 public class ShareModule extends ReactContextBaseJavaModule {
 
 
-  public ShareModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-  }
+    public ShareModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  public String getName() {
-      return "ReactNativeShareExtension";
-  }
+    @Override
+    public String getName() {
+        return "ReactNativeShareExtension";
+    }
 
-  @ReactMethod
-  public void close() {
-    getCurrentActivity().finish();
-  }
+    @ReactMethod
+    public void close() {
+        getCurrentActivity().finish();
+    }
 
-  @ReactMethod
-  public void data(Promise promise) {
-      promise.resolve(processIntent());
-  }
+    @ReactMethod
+    public void data(Promise promise) {
+        promise.resolve(processIntent());
+    }
 
-  public WritableArray processIntent() {
-      WritableArray dataArrayMap = Arguments.createArray();
-      Set<String> mediaTypesSupported = new HashSet<String>();
-      mediaTypesSupported.add("video");
-      mediaTypesSupported.add("audio");
-      mediaTypesSupported.add("image");
+    public WritableArray processIntent() {
+        WritableArray dataArrayMap = Arguments.createArray();
+        Set<String> mediaTypesSupported = new HashSet<String>();
+        mediaTypesSupported.add("video");
+        mediaTypesSupported.add("audio");
+        mediaTypesSupported.add("image");
 
-      String type = "";
-      String action = "";
-      String typePart = "";
+        String type = "";
+        String action = "";
+        String typePart = "";
 
-      Activity currentActivity = getCurrentActivity();
+        Activity currentActivity = getCurrentActivity();
 
-      if (currentActivity != null) {
-        Intent intent = currentActivity.getIntent();
-        action = intent.getAction();
-        type = intent.getType();
-        if (type == null) {
-          type = "";
-        } else {
-          typePart = type.substring(0, type.indexOf('/'));
-        }
-        if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
-          WritableMap dataMap = Arguments.createMap();
-          dataMap.putString("type", type);
-          dataMap.putString("value", intent.getStringExtra(Intent.EXTRA_TEXT));
-          dataArrayMap.pushMap(dataMap);
-        }
-        else if (Intent.ACTION_SEND.equals(action) && (
-          mediaTypesSupported.contains(typePart)) ) {
-          WritableMap dataMap = Arguments.createMap();
-          dataMap.putString("type", type);
-          Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-          dataMap.putString("value", "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri));
-          dataArrayMap.pushMap(dataMap);
-        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && mediaTypesSupported.contains(typePart)) {
-            ArrayList<Uri> uris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-            for (Uri uri : uris) {
+        if (currentActivity != null) {
+            Intent intent = currentActivity.getIntent();
+            action = intent.getAction();
+            type = intent.getType();
+            if (type == null) {
+                type = "";
+            } else {
+                typePart = type.substring(0, type.indexOf('/'));
+            }
+            if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
                 WritableMap dataMap = Arguments.createMap();
                 dataMap.putString("type", type);
+                dataMap.putString("value", intent.getStringExtra(Intent.EXTRA_TEXT));
+                dataArrayMap.pushMap(dataMap);
+            } else if (Intent.ACTION_SEND.equals(action) && (
+                    mediaTypesSupported.contains(typePart))) {
+                WritableMap dataMap = Arguments.createMap();
+                dataMap.putString("type", type);
+                Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
                 dataMap.putString("value", "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri));
                 dataArrayMap.pushMap(dataMap);
+            } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && mediaTypesSupported.contains(typePart)) {
+                ArrayList<Uri> uris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+                for (Uri uri : uris) {
+                    WritableMap dataMap = Arguments.createMap();
+                    dataMap.putString("type", type);
+                    dataMap.putString("value", "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri));
+                    dataArrayMap.pushMap(dataMap);
+                }
             }
         }
-      }
-      return dataArrayMap;
-  }
+        return dataArrayMap;
+    }
 }
