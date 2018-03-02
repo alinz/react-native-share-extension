25737ada7dffe28cd708257104bef02bed97a5ec
Bart Arribe <barthelemy.arribe@mediapps.io>
2018-03-02 16:11:44 +0100

    keep image orientation in temp file saved

--- ios/ReactNativeShareExtension.m
+++ ios/ReactNativeShareExtension.m
@@ -60,7 +60,7 @@ RCT_REMAP_METHOD(data,
 }
 
 - (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSString *value, NSString* contentType, NSException *exception))callback {
-    
+
     @try {
         NSExtensionItem *item = [context.inputItems firstObject];
         NSArray *attachments = item.attachments;
@@ -92,7 +92,7 @@ RCT_REMAP_METHOD(data,
             }];
         } else if (imageProvider) {
             [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                
+
                 /**
                  * Save the image to NSTemporaryDirectory(), which cleans itself tri-daily.
                  * This is necessary as the iOS 11 screenshot editor gives us a UIImage, while
@@ -100,11 +100,11 @@ RCT_REMAP_METHOD(data,
                  * Therefore the solution is to save a UIImage, either way, and return the local path to that temp UIImage
                  * This path will be sent to React Native and can be processed and accessed RN side.
                 **/
-                
+
                 UIImage *sharedImage;
                 NSString *filePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"RNSE_TEMP_IMG"];
                 NSString *fullPath = [filePath stringByAppendingPathExtension:@"png"];
-                
+
                 if ([(NSObject *)item isKindOfClass:[UIImage class]]){
                     sharedImage = (UIImage *)item;
                 }else if ([(NSObject *)item isKindOfClass:[NSURL class]]){
@@ -112,9 +112,9 @@ RCT_REMAP_METHOD(data,
                     NSData *data = [NSData dataWithContentsOfURL:url];
                     sharedImage = [UIImage imageWithData:data];
                 }
-                
-                [UIImagePNGRepresentation(sharedImage) writeToFile:fullPath atomically:YES];
-                
+
+                [UIImageJPEGRepresentation(sharedImage, 1.0) writeToFile:filePath atomically:YES];
+
                 if(callback) {
                     callback(fullPath, [fullPath pathExtension], nil);
                 }
