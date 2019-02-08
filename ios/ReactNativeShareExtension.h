e1df8f805a031fc2d8b84783781467ff2622c0f4
Robert Scott <keyboard.operator@gmail.com>
2019-02-08 10:45:54 -0800

    Reuse RCTBridge for the share view. Fixes #64

--- ios/ReactNativeShareExtension.h
+++ ios/ReactNativeShareExtension.h
@@ -2,5 +2,19 @@
 #import "React/RCTBridgeModule.h"
 
 @interface ReactNativeShareExtension : UIViewController<RCTBridgeModule>
+
+/**
+ * @deprecated This method should not be used unless you are creating
+ * a shared bridge inside of shareView.
+ * @note Please use @code shareViewWithRCTBridge @endcode instead.
+ */
 - (UIView*) shareView;
+
+/**
+ * Create a shareView using a common RCTBridge. The RCTBridge is reused
+ * for each launch of the share sheet. This allows the share sheet to
+ * free its resources in between launches.
+ */
+- (UIView*) shareViewWithRCTBridge:(RCTBridge*)sharedBridge;
+
 @end
