d6ff1b957674f8da7d461622dc22e4a12af16890
sandeeparoujo <sandeep@sgiggles.com>
2020-06-01 16:28:56 -0400

    add missing import

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -5,6 +5,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.Arguments;
 
 import android.app.Activity;
